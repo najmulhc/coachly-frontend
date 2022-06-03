cc6bcf93f4c2602484b7b4485f9c6faff050e33e
kridi32 <kridi32@outlook.com>
2022-06-03 12:02:07 +0600

    header and footer is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <div className=" bg-base-100">
-       
       <Navbar />
- 
+      <Footer/> 
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,92 @@
 import React from "react";
+import { GiTicket } from "react-icons/gi";
 import navLogo from "../img/navLogo.png";
 
 const Navbar = () => {
+  const links = (
+    <>
+      <li>
+                <a className="hover:text-gray-200" href="#">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="hover:text-gray-200" href="#">
+                  Catagory
+                </a>
+              </li>
+              <li>
+                <a className="hover:text-gray-200" href="#">
+                  Collections
+                </a>
+              </li>
+              <li>
+                <a className="hover:text-gray-200" href="#">
+                  Contact Us
+                </a>
+              </li>
+    </>
+  )
   return (
-    <div className=" bg-base-100">
-          <div className="navbar container mx-auto py-4">
-          <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+    <div className="flex flex-wrap place-items-center  ">
+      <section className="  mx-auto">
+        <nav className="flex justify-between bg-base-100 text-accent w-screen">
+          <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+            <a className="text-3xl font-bold font-heading" href="#">
+              <img className="h-9" src={navLogo} alt="logo" />
+            </a>
+            <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+              { links}
+            </ul>
+            <div className="hidden xl:flex items-center space-x-5  ">
+              <a className="hover:text-gray-200" href="#">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </a>
+              <a className="flex items-center hover:text-gray-200" href="#">
+                <GiTicket className="w-6 h-6"/>
+                
+              </a>
+              <a className="flex items-center hover:text-gray-200" href="#">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 hover:text-gray-200"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <a className="xl:hidden flex items-center" href="#">
+          <GiTicket className="w-6 h-6"/>
+            <div className="dropdown dropdown-left flex  h-full ml-2 ">
+          <label
+            tabIndex="0"
+            className="navbar-burger self-center mr-12 xl:hidden"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-6 w-6 hover:text-gray-200"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -19,82 +95,21 @@ const Navbar = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
+                d="M4 6h16M4 12h16M4 18h16"
               />
             </svg>
-          </label>
-          <ul
-            tabIndex="0"
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex="0">
-              <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-                  <a className="btn btn-ghost normal-case text-xl"><img src={navLogo} alt="navigation logo" width={180}/></a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex="0">
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
+            <ul
+              tabIndex="0"
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+            {links}
             </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn btn-primary">Get started</a>
-      </div>
-      </div>
+          </label>
+        </div>
+          </a>
+          
+        </nav>
+      </section>
     </div>
   );
 };
