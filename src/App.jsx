75a6e36068d9e13012e702e0fbc34fe52abc3c93
kridi32 <kridi32@outlook.com>
2022-06-04 12:56:39 +0600

    router setup and image added to footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import footerImg from "./img/footerImg.png";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 const App = () => {
   return (
     <div className=" bg-base-100">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/register" element={<Register /> } />
+      </Routes>
+     <div className="container max-w-sm mx-auto"> <img src={footerImg} className="w-full mb-[-5px] " alt="" /></div>
       <Footer/> 
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,45 +1,46 @@
 import React from "react";
 import { GiTicket } from "react-icons/gi";
+import { Link } from "react-router-dom";
 import navLogo from "../img/navLogo.png";
 
 const Navbar = () => {
   const links = (
     <>
       <li>
-                <a className="hover:text-gray-200" href="#">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a className="hover:text-gray-200" href="#">
-                  Catagory
-                </a>
-              </li>
-              <li>
-                <a className="hover:text-gray-200" href="#">
-                  Collections
-                </a>
-              </li>
-              <li>
-                <a className="hover:text-gray-200" href="#">
-                  Contact Us
-                </a>
-              </li>
+        <Link to="/" className="hover:text-secondary" >
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/" className="hover:text-secondary" >
+          Catagory
+        </Link>
+      </li>
+      <li>
+        <Link to="/" className="hover:text-secondary" >
+          Collections
+        </Link>
+      </li>
+      <li>
+        <Link to="/" className="hover:text-secondary" >
+          Contact Us
+        </Link>
+      </li>
     </>
-  )
+  );
   return (
     <div className="flex flex-wrap place-items-center  ">
       <section className="  mx-auto">
-        <nav className="flex justify-between bg-base-100 text-accent w-screen">
+        <nav className="flex justify-between  bg-base-100 text-accent w-screen">
           <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-            <a className="text-3xl font-bold font-heading" href="#">
+            <Link to="/" className="text-3xl font-bold font-heading" >
               <img className="h-9" src={navLogo} alt="logo" />
-            </a>
+            </Link>
             <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-              { links}
+              {links}
             </ul>
             <div className="hidden xl:flex items-center space-x-5  ">
-              <a className="hover:text-gray-200" href="#">
+              <Link to="/" className="hover:text-secondary" >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6"
@@ -54,15 +55,14 @@ const Navbar = () => {
                     d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                   />
                 </svg>
-              </a>
-              <a className="flex items-center hover:text-gray-200" href="#">
-                <GiTicket className="w-6 h-6"/>
-                
-              </a>
-              <a className="flex items-center hover:text-gray-200" href="#">
+              </Link>
+              <Link to="/" className="flex items-center hover:text-secondary" >
+                <GiTicket className="w-6 h-6" />
+              </Link>
+              <Link to="/" className="flex items-center hover:text-secondary" >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 hover:text-gray-200"
+                  className="h-6 w-6 hover:text-secondary"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -74,40 +74,39 @@ const Navbar = () => {
                     d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
-              </a>
+              </Link>
             </div>
           </div>
-          <a className="xl:hidden flex items-center" href="#">
-          <GiTicket className="w-6 h-6"/>
+          <Link to="/" className="xl:hidden flex items-center" >
+            <GiTicket className="w-6 h-6" />
             <div className="dropdown dropdown-left flex  h-full ml-2 ">
-          <label
-            tabIndex="0"
-            className="navbar-burger self-center mr-12 xl:hidden"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 hover:text-gray-200"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-            <ul
-              tabIndex="0"
-              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-            {links}
-            </ul>
-          </label>
-        </div>
-          </a>
-          
+              <label
+                tabIndex="0"
+                className="navbar-burger self-center mr-12 xl:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 hover:text-secondary"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+                <ul
+                  tabIndex="0"
+                  className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  {links}
+                </ul>
+              </label>
+            </div>
+          </Link>
         </nav>
       </section>
     </div>
