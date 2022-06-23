d194ec4b346ca0df58cafe47c6635a740e2739ea
najmulhc <kridi32@outlook.com>
2022-06-23 16:59:04 +0600

    google like input field prototype is ready

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,18 @@
-const Register = ({setAuthType}) => {
-    return (
-        <>
-        <h1>This is register page</h1>
-        <button className="btn btn-primary" type="button" onClick={() => setAuthType(true)}>LOgin</button>
-        </>
-    )
-}
+const Register = ({ setAuthType }) => {
+  return (
+    <>
+      <h1>This is register page</h1>
+      <button
+        className="btn btn-primary"
+        type="button"
+        onClick={() => setAuthType(true)}
+      >
+        LOgin
+      </button>
+      <label htmlFor="my-modal" type="button" className="btn btn-secondary">
+        Close Modal
+      </label>
+    </>
+  );
+};
 export default Register;
