9df6150f4a5387f1279199190fe46964335fe8e3
kridi32 <kridi32@outlook.com>
2022-07-05 21:13:57 +0600

    form info can be take by react hook form with basic validation

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,43 +1,63 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const login = (e) => {
-    e.preventDefault();
-    console.log(email, password);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const formSubmit = (data) => {
+    console.log(data);
   };
   return (
     <section>
       <h1 className="text-center text-4xl font-bold text-accent">Login</h1>
-      <form onSubmit={login} className="w-full">
+      <form onSubmit={handleSubmit(formSubmit)} className="w-full">
         <div className="w-full form-control relative">
           <label htmlFor="email" className="">
             <input
               type="email"
               name="email"
               placeholder=" "
-              onChange={(e) => setEmail(e.target.value)}
+              {...register("email", {
+                pattern: 'w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$',
+                required: true,
+              })}
               className="h-12 text-lg px-2 outline-none w-full my-4 border-2 border-primary rounded-md text-primary focus:border-secondary focus:text-secondary transition duration-300"
             />
-            <span className="input-email text-lg absolute left-4 top-[30px] mx-0 text-primary transition-all duration-200 ">
+            <span className="input-email text-lg absolute left-4 top-[27px] mx-0 text-primary transition-all duration-200 ">
               Email
             </span>
           </label>
           <label className="label">
-            <span className="label-text-alt mt-[-20px] text-red-700">this is not a valid email</span> 
+            <span className="label-text-alt mt-[-20px] text-red-700">
+              {errors.email?.type === "required" &&
+                "you need to enter your email adddres"}
+              {
+                errors.email?.type === "pattern" && "Please enter a valid email address"
+              }
+            </span>
           </label>
         </div>
         <label htmlFor="password" className="relative">
           <input
             type="password"
             name="password"
-            onChange={(e) => setPassword(e.target.value)}
             placeholder=" "
+            {...register("password", {
+              required: true,
+            })}
             className="h-12 text-lg px-2 outline-none w-full my-4 border-2 border-primary rounded-md text-primary focus:border-secondary focus:text-secondary transition duration-300"
           />
-          <span className="input-password text-lg absolute left-4 top-0 mx-0 text-primary transition-all duration-200 ">
+          <span className="input-password text-lg absolute left-4 top-[-2px] mx-0 text-primary transition-all duration-200">
             Password
+          </span>
+        </label>
+        <label className="label" >
+          <span className="label-text-alt mt-[-20px] text-red-700">
+            {errors.password?.type === "required" &&
+              "Please enter your password for login"}
           </span>
         </label>
         <button className="btn btn-primary mx-auto" type="submit">
